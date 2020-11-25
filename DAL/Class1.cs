20e564c8cc8e48608ca00660ded5f29b801d00d2
Freek van Lier <freek.vlier@gmail.com>
2020-11-25 15:58:51 +0100

    Add project files.

---
+++ DAL/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DAL
+{
+    public class Class1
+    {
+        public void test()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Server=mssql.fhict.local;Database=dbi454449;User Id=dbi454449;Password=454449ICT;");
+                connection.Open();
+
+                string SQL = "SELECT * FROM Persons";
+                SqlCommand command = new SqlCommand(SQL, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("SUCCESS-----------------------------");
+                    Console.WriteLine(reader["FirstName"]);
+                    Console.WriteLine("SUCCESS-----------------------------");
+                }
+            }
+            catch (SqlException error)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine(error);
+                Console.WriteLine("-----------------------------");
+            }
+
+
+        }
+    }
+}
---
+++ Logic/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DAL;
+
+namespace Logic
+{
+    public class Class2
+    {
+        public void testje()
+        {
+            Class1 poep = new Class1();
+            poep.test();
+        }
+    }
+}
